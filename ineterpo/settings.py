73131a783fd2c0916501d2d51e9dab052e905b86
Alexander <caicedoficial@gmail.com>
2025-02-23 18:27:54 -0500

    second commit

--- ineterpo/settings.py
+++ ineterpo/settings.py
@@ -1,12 +1,8 @@
 from pathlib import Path
-from dotenv import load_dotenv
-import os
-
-load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = os.getenv("SECRET_KEY")
+SECRET_KEY = "django-insecure-2ye&_9@aev(f(8r&f$$e!o(*ycqi02b+-&r&utixj7a6(iyzws"
 
 DEBUG = False
 
