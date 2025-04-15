d07021fb9345eb36ad2151e0eda6b3c355688914
Alexander <caicedoficial@gmail.com>
2025-04-15 12:17:25 -0500

    Añadir permisos de ejecución a build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-python -m pip install --upgrade pip
-pip install -r requirements.txt
-python manage.py collectstatic --noinput
+/opt/vercel/python3/bin/python3 -m pip install --upgrade pip
+/opt/vercel/python3/bin/pip3 install -r requirements.txt
+/opt/vercel/python3/bin/python3 manage.py collectstatic --noinput
