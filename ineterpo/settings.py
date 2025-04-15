a19f2498b6dfa2341d633666763a6769f92ae915
Alexander <caicedoficial@gmail.com>
2025-04-15 11:34:34 -0500

    Remove unused video file, update requirements to include psycopg2 and python-dotenv, and enhance image handling in templates by adding aspect-auto class for better responsiveness.

--- ineterpo/settings.py
+++ ineterpo/settings.py
@@ -1,4 +1,9 @@
 from pathlib import Path
+from dotenv import load_dotenv
+import os
+
+# Cargar las variables de entorno desde el archivo .env
+load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -91,8 +96,12 @@ WSGI_APPLICATION = "ineterpo.wsgi.app"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": os.getenv("DB_ENGINE"),
+        "NAME": os.getenv("DB_NAME"),
+        "HOST": os.getenv("DB_HOST"),
+        "PORT": os.getenv("DB_PORT"),
+        "USER": os.getenv("DB_USER"),
+        "PASSWORD": os.getenv("DB_PASSWORD"),
     }
 }
 
