8d61a1368fe3e0abc4dd5236abc822dda071334f
Alexander <caicedoficial@gmail.com>
2025-04-15 12:40:39 -0500

    Actualizar configuración de DEBUG en settings.py de True a False

--- ineterpo/settings.py
+++ ineterpo/settings.py
@@ -9,7 +9,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = "django-insecure-2ye&_9@aev(f(8r&f$$e!o(*ycqi02b+-&r&utixj7a6(iyzws"
 
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [".vercel.app", "localhost", "127.0.0.1"]
 
