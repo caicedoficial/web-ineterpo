7573dbb316ab0b935cca65e6d9faeb43eea336b8
Alexander <caicedoficial@gmail.com>
2025-04-15 12:48:52 -0500

    Actualizar rutas en vercel.json y modificar build.sh para usar python3 directamente

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-/opt/vercel/python3/bin/python3 -m pip install --upgrade pip
-/opt/vercel/python3/bin/pip3 install -r requirements.txt
-/opt/vercel/python3/bin/python3 manage.py collectstatic --noinput
+python3 -m pip install --upgrade pip
+pip3 install -r requirements.txt
+python3 manage.py collectstatic --noinput
