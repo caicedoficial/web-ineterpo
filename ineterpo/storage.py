8f9b76eda911afd8d5b77acf7feefe59e5a4d11d
Alexander <caicedoficial@gmail.com>
2025-04-15 11:57:56 -0500

    Agregar manejo de tipos MIME en SupabaseStorage para asegurar la correcta carga de archivos

--- ineterpo/storage.py
+++ ineterpo/storage.py
@@ -3,6 +3,7 @@ from django.conf import settings
 from supabase import create_client
 import os
 import io
+import magic
 
 class SupabaseStorage(Storage):
     def __init__(self):
@@ -11,14 +12,16 @@ class SupabaseStorage(Storage):
             settings.SUPABASE_KEY
         )
         self.bucket_name = settings.SUPABASE_BUCKET_NAME
+        self.mime = magic.Magic(mime=True)
 
     def _save(self, name, content):
         file_data = content.read()
+        content_type = self.mime.from_buffer(file_data)
         
         self.supabase.storage.from_(self.bucket_name).upload(
             path=name,
             file=file_data,
-            file_options={"contentType": content.content_type}
+            file_options={"contentType": content_type}
         )
         
         return name
