49a9428d152fb63c2594dc9cdc91cc704f2e45d3
Alexander <caicedoficial@gmail.com>
2025-04-19 12:08:35 -0500

    Actualizar ALLOWED_HOSTS en settings.py y agregar dependencias en requirements.txt

--- ineterpo/settings.py
+++ ineterpo/settings.py
@@ -11,7 +11,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 
 DEBUG = False
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
+ALLOWED_HOSTS = [".vercel.app", "localhost", "127.0.0.1"]
 
 INSTALLED_APPS = [
     "django.contrib.admin",
