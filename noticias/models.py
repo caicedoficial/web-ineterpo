4830f632ce5fac688459a76cea956add7845938e
Alexander <caicedoficial@gmail.com>
2025-04-24 16:35:06 -0500

    Agregar campos 'archivo_url' y 'tipo_archivo' al modelo ArchivosNoticias, implementar validaciones y crear plantilla para renderizar archivos multimedia.

--- noticias/models.py
+++ noticias/models.py
@@ -1,9 +1,9 @@
 from django.db import models
-from django.urls import reverse
 from django.utils import timezone
 from uuid import uuid4
 import os
 from tinymce.models import HTMLField
+from django.core.exceptions import ValidationError
 
 TIPOS_NOTICIAS = (
     ('General', 'General'),
@@ -51,10 +51,21 @@ class ArchivosNoticias(models.Model):
         filename = f'{uuid4()}.{ext}'
         return os.path.join(f'noticias/{instance.noticia.fecha.year}/{instance.noticia.fecha.month}/{instance.noticia.fecha.day}/{instance.noticia.id}/', filename)
     
-    archivo = models.FileField(upload_to=archivo_ruta, blank=True, null=True)
     archivo_url = models.URLField("Enlace directo al archivo", max_length=500, blank=True, null=True,
                                    help_text="Pega aquí la URL si el archivo es muy grande o ya está en S3.")
+    if archivo_url:
+        archivo = models.FileField(archivo_url, blank=True, null=True, help_text="Si el archivo es muy grande o ya está en S3, pega aquí la URL.")
+
+    archivo = models.FileField(upload_to=archivo_ruta, blank=True, null=True)
     tipo_archivo = models.CharField(max_length=20, choices=TIPO_ARCHIVO_CHOICES)
+
+    def clean(self):
+        # Evitar que se llenen ambos campos
+        if self.archivo and self.archivo_url:
+            raise ValidationError("No puedes subir un archivo y también ingresar una URL. Usa solo uno.")
+        # Evitar que ambos estén vacíos
+        if not self.archivo and not self.archivo_url:
+            raise ValidationError("Debes subir un archivo o ingresar una URL.")
 
     def es_imagen(self):
         return self.archivo.name.lower().endswith(('.png', '.jpg', '.jpeg', '.gif', '.webp'))
